febb4ad56de5977612687591c7748ae364544553
XiaocongDong <buptdxc@gmail.com>
2020-11-17 15:58:09 +0800

    feat: add http server

---
+++ httpServer.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const { hostname } = require('os')
+const port = 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World')
+})
+
+server.listen(port, hostname, () => {
+  console.log(`server running at ${port}`)
+})
